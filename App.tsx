f2eb5d5753826262115f55b0e590d0c5cd83f9e1
anb-sree <annabathulabhavyasri@gmail.com>
2025-12-19 00:28:56 +0530

    Phase 0: React Native + Firebase Android setup

--- App.tsx
+++ App.tsx
@@ -6,7 +6,9 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
@@ -23,22 +25,46 @@ function App() {
   );
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+// function AppContent() {
+//   const safeAreaInsets = useSafeAreaInsets();
 
+//   return (
+//     <View style={styles.container}>
+//       <NewAppScreen
+//         templateFileName="App.tsx"
+//         safeAreaInsets={safeAreaInsets}
+//       />
+//     </View>
+//   );
+// }
+
+
+function AppContent() {
+  firestore();
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Text style={styles.text}>
+        VisionGuideAI is running 🚀
+      </Text>
     </View>
   );
 }
 
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+// });
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 22,
+    textAlign: 'center',
   },
 });
 
